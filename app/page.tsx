af5909efcd821c31902207f3a465a3647bf431ab
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-01-15 08:47:44 +0530

    hero image top padding bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import SectionDivider from "@/components/helpers/SectionDivider";
 
 export default function Home() {
   return (
-    <main className={`flex flex-col items-center pt-48 z-10 px-10 sm:px-16 md:px-28 lg:px-44 xl:px-60 2xl:px-84`}>
+    <main className={`flex flex-col items-center pt-28 md:pt-36 lg:pt-44 z-10 px-6 sm:px-16 md:px-28 lg:px-44 xl:px-60 2xl:px-84`}>
       <Hero />
       <SectionDivider />
       <About />
